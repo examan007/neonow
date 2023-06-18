39e8e56a6144096a4507d2925e19d61b90e7254f
James Hayes <exajames@gmail.com>
2023-06-18 01:44:09 +0000

    new flow

--- js/login.js
+++ js/login.js
@@ -56,7 +56,7 @@
         return value
       }
       function getNextForm(section, flag) {
-        //console.log("$$$%%%$$$ getNextForm $$$")
+        console.log("$$$%%%$$$ getNextForm $$$ section=[" + section + "]")
         $('#login-window').css("display", "none")
         $('.Dialogue').each( function () {
             $(this).css("display", "none")
@@ -67,10 +67,20 @@
             setCookieInParent("", flag)
         }
         try {
-            $("#" + section).css("display", "block")
-            if (section != 'empty') {
+            function testSectionName(section) {
+                if (typeof(section) === 'undefined') {
+                    return empty
+                } else {
+                    return section
+                }
+            }
+            if (testSectionName(section) != 'empty') {
                 $('#login-window').css("display", "block")
+                $("#" + section).css("display", "block")
+                checkInputContainerHelper(section)
                 showlogin()
+            } else {
+                exitlogin()
             }
         } catch (e) {
         }
@@ -87,11 +97,20 @@
                 }
                 return ""
             }
-            const formData = new URLSearchParams(getParameters('#Login-form', "?") +
-                extraparams
-                )
-            const extData = new URLSearchParams(getParameters(extended, "?"))
+            function getExtraParams(form, params) {
+                try {
+                    if (params.length > 0) {
+                        form.append(params.split('=')[0], params.split('=')[1])
+                    }
+                } catch (e) {
+                    console.log(e.toString())
+                }
+            }
+
+            const formData = new URLSearchParams(getParameters('#Login-form', "?"))
+            getExtraParams(formData, extraparams)
             try {
+                const extData = new URLSearchParams(getParameters(extended, "?"))
                 extData.forEach((value, key) => {
                     if (formData.get(key) != null) {
                         formData.set(key, value)
@@ -100,11 +119,11 @@
                     }
                 });
             } catch {
-                console.log(e.toString())
+                console.log("get extended parmas: " + e.toString())
             }
             return formData
         }
-            function getCredentials(formData) {
+        function getCredentials(formData) {
             const credential = formData.get('username') + ":" + formData.get('password')
             console.log("credential=[" + credential + "]")
             console.log("Login-form=[" + formData.toString() + "]");
@@ -112,7 +131,7 @@
         }
         function getBooks(extended) {
             console.log("getBooks() ...")
-            formData = getForms("", extended)
+            const formData = getForms("", extended)
             const xhr = executeAJAX((jsondata)=> {
                 window.parent.postMessage(JSON.stringify({
                     operation: 'readappointments',
@@ -129,35 +148,13 @@
                 xhr.send(data);
             } catch (e) {
                 console.log(e.toString())
-                showlogin()
+                //showlogin()
             }
         }
       function setEmail(templatename, extended) {
           getNextForm("empty")
-          function getParameters(formname, delim) {
-            try {
-               const formext = $(formname).serialize();
-               if (formext.length > 0) {
-                    return delim + formext
-               }
-            } catch (e) {
-                console.log(e.toString())
-            }
-            return ""
-          }
-          const formData = new URLSearchParams(getParameters('#Login-form', "?") +
-            '&templatename=' + templatename
-            )
-          const extData = new URLSearchParams(getParameters(extended, "?"))
-          extData.forEach((value, key) => {
-            if (formData.get(key) != null) {
-                formData.set(key, value)
-            } else {
-                formData.append(key, value);
-            }
-          });
-
-          thisemail = formData.get('username')
+          const formData = getForms('templatename=' + templatename, extended)
+           thisemail = formData.get('username')
           function getNextSectionForm() {
             const nextform = formData.get('nextform')
             if (nexform == null) {
@@ -215,6 +212,8 @@
               if (templatename !== "verification.html") {
                   //exitlogin()
                   try {
+                    console.log("message: [" + JSON.stringify(jsonmsg) + "]")
+                    setInputValues(jsonmsg)
                     getNextForm(jsonmsg.request.nextform)
                   } catch (e) {
                     getNextForm('Verify')
@@ -257,7 +256,9 @@
         console.log("message posted [" + JSON.stringify(message) + "]")
       }
 
-      function getAuthenticationCookie() {
+      function getAuthenticationCookie(extended) {
+          const formData = getForms("", extended)
+          console.log("formData=[" + formData.toString() + "]")
           const xhr = new XMLHttpRequest();
           xhr.timeout = 5000;
           const url = 'https://test.neolation.com/auth';
@@ -272,9 +273,6 @@
             }
           }
           function onLoadAuth () {
-              const formRaw = $('#Login-form').serialize();
-              console.log("formRaw=[" + formRaw + "]")
-              const formData = new URLSearchParams("?" + $('#Login-form').serialize())
               return {
                 thisemail: formData.get('username'),
                 onLoad: function () {
@@ -306,22 +304,43 @@
                         response = JSON.parse(xhr.response)
                         const email = response.email
                         const token = response.token
+                        const templatename = getTemplateName()
+                        function getTemplateName() {
+                            const temp = formData.get('template')
+                            if (temp == null) {
+                            } else
+                            if (typeof(temp) === 'undefined') {
+                            } else
+                            if (temp.length > 0) {
+                                return temp
+                            }
+                            return 'verification.html'
+                        }
                         if (typeof(token) !== "undefined") {
+                          console.log("Token found.")
                           setCookieInParent(token)
                           $("#neotoken").val(token)
+                          if (typeof(extended) !== 'undefined') {
+                            setEmail(templatename, extended)
+                          }
                         } else
                         if (testForNeoToken()) {
                            $("#renewflag").val(false)
                            console.log("token found status=[" + xhr.status + "]")
                         } else
                         if (typeof(email) !== "undefined") {
-                          showHeader()
-                          setEmail('verification.html')
+                            console.log("email defined")
+                            showHeader()
+                            if (typeof(extended) !== 'undefined') {
+                              setEmail(templatename, extended)
+                            } else {
+                              setEmail('verification.html')
+                            }
                         } else {
                            console.log("status=[" + xhr.status + "]")
                         }
                       } catch (e) {
-                        console.log(e.toString())
+                        console.log(e.stack.toString())
                       }
                     } else
                     if (xhr.status === 401) {
@@ -345,7 +364,6 @@
           console.log("getAuthenticationCookie() with [" + thisemail + "]")
           const credential = thisemail + ":" + 'blockade'
           xhr.setRequestHeader("Authorization", "Basic " + btoa(credential))
-          const formData = new URLSearchParams("?" + $('#Login-form').serialize())
           xhr.send(formData.toString())
       }
         function exitlogin() {
@@ -419,8 +437,10 @@
         }
         function setOldDateTime(datetime) {
             try {
-                const input = document.getElementById('olddatetime')
-                input.value = datetime
+                var elements = document.querySelectorAll('input[name="olddatetime"]');
+                elements.forEach((input)=> {
+                    input.value = datetime
+                })
             } catch (e) {
                 console.log(e.toString())
             }
@@ -458,15 +478,14 @@
            console.log("origin=[" + JSON.stringify(event) + "]")
            if (event.isTrusted === true) {
               // Process the message data
-
               var message = event.data;
               console.log("Received messageL:", message);
               try {
                 const jsonmsg = JSON.parse(message)
                 if (jsonmsg.operation === 'showsection') {
                     try {
+                        console.log("message: [" + JSON.stringify(jsonmsg) + "]")
                         setInputValues(jsonmsg)
-                        initHelpedInput('usermessage','message-box')
                         $("#login-window").css("top", jsonmsg.message.ypos + "px");
                         if (getWindowDimensions().width > 800) {
                             $("#login-window").css("left", "550px");
@@ -474,7 +493,15 @@
                     } catch (e) {
                         console.log("showsection: " + e.toString())
                     }
-                    getNextForm(jsonmsg.sectionname)
+                    function getSectionName() {
+                        const section = jsonmsg.sectionname
+                        if (false) { //section === 'Request') {
+                            return 'Appoint'
+                        } else {
+                            return section
+                        }
+                    }
+                    getNextForm(getSectionName())
                 } else
                 if (jsonmsg.operation === 'showstatus') {
                     getNextForm('Status')
@@ -488,7 +515,7 @@
                     getBooks()
                 }
               } catch (e) {
-                console.log(e.toString())
+                console.log(e.stack.toString())
                 console.log(event.toString())
              }
            }
@@ -505,16 +532,94 @@
                 window.parent.postMessage(JSON.stringify(message), "*")
             })
         }
-        registerForEvents()
+        function setServices(newvalue) {
+            const services = document.querySelectorAll('input[name="services"]')
+            services.forEach((element)=> {
+                console.log("services=[" + newvalue + "]")
+                element.value = newvalue
+                element.checkValue()
+            })
+        }
+        function getServices() {
+            try {
+                const services = document.querySelectorAll('input[name="services"]')
+                return services[0].value
+            } catch (e) {
+                console.log(e.toString())
+            }
+            return ""
+        }
+        function checkInputValue() {
+            console.log("checkValue " + this.value)
+            if (this.value.length > 0) {
+                console.log("value exists.")
+                this.helper.classList.add("faded");
+            } else {
+                this.helper.classList.remove("faded");
+            }
+        }
+        function clickInputValue() {
+            console.log("clickInputValue()")
+            var services = getServices()
+            if (this.value.length > 0) {
+                this.helper.classList.remove("faded");
+                window.setTimeout( ()=> { this.value = "" }, 500)
+                services = services.replace(this.value, "").trim()
+            } else {
+                this.helper.classList.add("faded");
+                this.value = this.helper.value
+                if (services.length > 0) {
+                    services = services + " " + this.value
+                } else {
+                    services = this.value
+                }
+            }
+            setServices(services)
+        }
+        function checkInputContainerHelper(sectionname) {
+            const section = document.getElementById('#' + sectionname)
+            initInputContainerHelper(section, (input)=> {
+                input.checkValue()
+            }, "input-box", checkInputValue)
+        }
+        function initInputContainerHelper(parent, register, inputclass, checkmethod) {
+            function getElements() {
+                if (parent == null) {
+                    return document.querySelectorAll('.input-container')
+                } else {
+                    return parent.querySelectorAll('.input-container')
+                }
+            }
+            const elements = getElements()
+            elements.forEach((element)=> {
+                try {
+                    const inputs = element.getElementsByTagName('input')
+                    const input = inputs[0]
+                    if (input.classList.contains(inputclass)) {
+                        input.helper = inputs[1]
+                        input.checkValue = checkmethod
+                        register(input)
+                    }
+                } catch (e) {
+                    console.log(e.stack.toString())
+                }
+            })
+        }
 
     return {
         onload: function () {
            console.log("load href=[" + window.location.href + "]")
+            registerForEvents()
+            initInputContainerHelper(null, (input)=> {
+                input.addEventListener("input", input.checkValue)
+                input.checkValue()
+            }, "input-box", checkInputValue )
+            initInputContainerHelper(null, (input)=> {
+                input.addEventListener("click", input.checkValue)
+            }, "input-select", clickInputValue)
+
             const serverurl = getQueryValue('serverurl')
             urlemail = getQueryValue('username')
-            initHelpedInput('username','input-box')
-            initHelpedInput('usermessage','message-box')
-            //thisemail = localStorage.getItem('email');
             getQueryValue('password')
 
             $('#username').focus()
@@ -569,14 +674,35 @@
         getAuthenticationCookie: function () {
             getAuthenticationCookie()
         },
+        verifyAppointment: function () {
+            try {
+                var sourceElement = document.getElementById("email");
+                var targetElement = document.getElementById("username");
+                var sourceValue = sourceElement.getAttribute("value");
+                targetElement.setAttribute("value", 'james.hayes@neolation.com');
+            } catch (e) {
+                console.log(e.stack.toString())
+            }
+            getAuthenticationCookie('#Appoint-form')
+        },
         requestAppointment: function () {
             setEmail('confirmation.html', '#Request-form')
         },
         changeAppointment: function () {
             setEmail('cancellation.html', '#Change-form')
         },
-        showForm(sectionname) {
+        showForm: function (sectionname) {
             getNextForm(sectionname)
+        },
+        LastPanel: "",
+        setServices: function (nextpanel) {
+            console.log("setServices(); ")
+            LastPanel = nextpanel
+            getNextForm("Select")
+        },
+        doneServices: function () {
+            console.log("doneServices(); ")
+            getNextForm(LastPanel)
         }
     }
 }
